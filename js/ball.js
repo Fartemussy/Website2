31af1a0e9d3774dda6316537d217259c5a895b6e
Fartemussy <articlechicken@gmail.com>
2025-04-10 23:50:35 -0700

    ah

---
+++ js/ball.js
@@ -0,0 +1,129 @@
+const windowHeight = window.innerHeight
+const windowWidth = window.innerWidth
+
+
+const ball = document.createElement('div')
+document.body.appendChild(ball)
+const ballRadius = 40
+let ballXPosition = windowWidth / 2 - ballRadius
+let ballYPosition = windowHeight / 2 - ballRadius
+let ballSpeed = 5
+let ballXDirection = 1
+let ballYDirection = 1
+
+const LPaddle = document.createElement('div')
+document.body.appendChild(LPaddle)
+let LPaddleWidth = 20
+let LPaddleHeight = 100
+let LPaddleSpeed = 10
+let LPaddleYPosition = windowHeight / 2 - LPaddleHeight / 2
+let LPaddleXPosition = 70
+
+let score = 0 // Display score and increase score by 1 every time ball hits the paddle
+let level = 1 // display level and increase level by 1 every 10 scores
+// as level increase, increase ball speed
+// if ball gets past the paddle, end the game: ball can stop or disappear
+// optional: sound fx
+
+const scoreText = document.createElement('div')
+document.body.appendChild(scoreText)
+
+createBall()
+
+function moveBall(){
+    ballXPosition = ballXPosition + ballSpeed * ballXDirection
+    ballYPosition = ballYPosition + ballSpeed * ballYDirection
+    ball.style.left = `${ballXPosition}px`
+    ball.style.top = `${ballYPosition}px`
+    if (ballYPosition < 0 || ballYPosition > windowHeight - 2 * ballRadius) {
+        ballYDirection = ballYDirection * -1
+    }
+    if (ballXPosition < 0 || ballXPosition > windowWidth - 2 * ballRadius) {
+        ballXDirection = ballXDirection * -1
+    }
+    let ballTop = ballYPosition
+    let ballBottom = ballYPosition + 2 * ballRadius
+    let ballLeft = ballXPosition
+    let LPaddleTop = LPaddleYPosition
+    let LPaddleBottom = LPaddleYPosition + LPaddleHeight
+    let LPaddleRight = LPaddleXPosition + LPaddleWidth
+
+    if((ballBottom >= LPaddleTop) && (ballTop <= LPaddleBottom) && (ballLeft <= LPaddleRight) && (ballXDirection == -1) ){
+            ballXDirection = ballXDirection * -1
+        }
+}
+
+
+function createBall(){
+    ball.style.height = `${2*ballRadius}px`
+    ball.style.width = `${2*ballRadius}px`
+    ball.style.borderRadius = "50%"
+    ball.style.backgroundColor = "pink"
+    ball.style.position = "absolute"
+    ball.style.top = `${windowHeight/2 - ballRadius}px`
+    ball.style.left = `${windowWidth/2 - ballRadius}px`
+}
+
+createLPaddle()
+function createLPaddle() {
+    LPaddle.style.height = `${LPaddleHeight}px`
+    LPaddle.style.width = `${LPaddleWidth}px`
+    LPaddle.style.backgroundColor = 'blue'
+    LPaddle.style.position = 'absolute'
+    LPaddle.style.left = `${LPaddleXPosition}px`
+    LPaddle.style.top = `${LPaddleYPosition}px`
+}
+
+wKey = false
+sKey = false
+
+document.addEventListener('keydown', (event)=> {
+    if (event.key == 'w') {
+        wKey = true
+        console.log(wKey)
+    }
+    if (event.key == 's') {
+        sKey = true
+    }
+})
+
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'w') {
+        console.log(wKey)
+        wKey = false
+    }
+    if (event.key == 's') {
+        sKey = false
+    }
+})
+
+function moveLPaddle(){
+    if(wKey == true && LPaddleYPosition > 0){
+        LPaddleYPosition = LPaddleYPosition - LPaddleSpeed
+    }
+    if (sKey == true && LPaddleYPosition < windowHeight - LPaddleHeight) {
+        LPaddleYPosition = LPaddleYPosition + LPaddleSpeed
+    }
+    LPaddle.style.top = `${LPaddleYPosition}px`
+}
+
+function animate(){
+    moveBall()
+    moveLPaddle()
+    requestAnimationFrame(animate)
+
+}
+
+animate()
+
+function createText(){
+    scoreText.innerHTML = Score: `${score}px`
+    scoreText.style.position = 'absolute'
+    scoreText.style.top = '30px'
+    scoreText.style.left = '50px'
+    scoreText.style.color = 'black'
+    scoreText.style.fontSize = '30px'
+
+}
+
+createText()
---
+++ js/relaxer.js
@@ -0,0 +1,24 @@
+const container = document.getElementById('container')
+const text = document.getElementById('text')
+
+breatheAnimation()
+setInterval(breatheAnimation, 8000)
+
+function breatheAnimation() {
+    text.innerHTML = 'breathe in'
+    container.classList.remove('shrink')
+    container.classList.add('grow')
+
+    setTimeout(() => {
+        text.innerHTML = 'hold'
+    }, 4000)
+
+    setTimeout(() => {
+        text.innerHTML = 'breathe out'
+        container.classList.remove('grow')
+        container.classList.add('shrink')
+    }, 6000)
+}
+
+breatheAnimation()
+breatheAnimation()
